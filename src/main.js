1169b28224ef9e7497d2a4a278babe6aa45fbf96
mreilly29 <mreilly18@jcu.edu>
2018-12-17 14:42:56 -0500

    added start and stop buttons

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 export default () => {
   const container = document.getElementById('container');
   const fireworks = new Fireworks(container);
-  fireworks.start();
+  const startButton = document.querySelector('#start');
+  const stopButton = document.querySelector('#stop');
+
+  startButton.addEventListener("click", () => {
+    console.log('you idiot');
+    fireworks.start();
+  });
+  stopButton.addEventListener("click", () => {
+    console.log('you idiot');
+    fireworks.stop();
+  });
+  
+
+  //fireworks.start();
 };
   import './main.css';
   import Fireworks from 'fireworks-canvas';
